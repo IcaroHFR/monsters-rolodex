a126dfcaee508f964af4340476ef98d9ede39f89
IcaroHFR <caro.henrique1997@gmail.com>
2020-09-25 00:20:50 -0300

    Updates

---
+++ precache-manifest.e687f192538940e080d9a2da91b120a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3e7facb986440f559c01b15e785dc23",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "b803bf3bd524a3b69388",
+    "url": "/monsters-rolodex/static/css/main.1285c60a.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b803bf3bd524a3b69388",
+    "url": "/monsters-rolodex/static/js/main.4994db83.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
